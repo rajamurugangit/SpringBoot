18884caa396aadcd2bfc96c854dffaf9f33f3481
Rajamurugan <166983670+rajamurugangit@users.noreply.github.com>
2024-05-16 21:51:31 +0530

    Delete ShoppingMallManagment directory

--- ShoppingMallManagment/src/main/java/com/tnsif/ShoppingMallManagment/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.ShoppingMallManagment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tnsif.ShoppingMallManagment.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-
-}
